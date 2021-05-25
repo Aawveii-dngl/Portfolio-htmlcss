264ef22ba085da0dcbd71c38dc80c44d4b92b4c5
Aawveii-dngl <dangolavi9999@gmail.com>
2021-05-25 23:24:23 +0545

    Animation added

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ console.log(navlist);
 button.addEventListener("click", () => {
   navlist.classList.toggle("show");
 });
+//It is for js animations
+window.addEventListener("scroll", () => {
+  let content1 = document.querySelector(".skills");
+  let currentposition1 = content1.getBoundingClientRect().top;
+  let screenposition1 = window.innerHeight / 1.5;
+  if (currentposition1 > screenposition1) {
+    content1.classList.add("active1");
+  } else {
+    content1.classList.remove("active1");
+  }
+});
+//Actually its creating problem in nav
